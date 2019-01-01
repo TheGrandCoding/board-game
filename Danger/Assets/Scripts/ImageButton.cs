84d2d5fa9f9b878c06f4b9197880ed81f5777686
CheAle14 <thedark1513@gmail.com>
2019-01-01 10:02:19 +0000

    Imlemented start of UI
    With extra fun

---
+++ Danger/Assets/Scripts/ImageButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public abstract class ImageButton : MonoBehaviour, IPointerClickHandler
+{
+	void Start () {
+        Startup();
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // TODO: get pixel at clicked place, ensure its alpha is set?
+        // so that we only trigger if they clicked on somewhere that isnt a background
+        // ie: only trigger if they actually click the image
+        Clicked();
+    }
+
+
+    public abstract void Clicked();
+    public abstract void Startup();
+}
---
+++ Danger/Assets/Scripts/TerritoryButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TerritoryButton : ImageButton {
+
+    public string Name;
+    public string ContinentName;
+    public int InternalID;
+
+    public Continent Continent {  get
+        {
+            return GameManager.Continents.FirstOrDefault(x => x.Name == ContinentName);
+        } }
+    public Territory Territory {  get
+        {
+            return Continent.GetTerritory(InternalID);
+        } }
+
+    public override void Clicked()
+    {
+        Debug.Log("Clicked on " + Continent.Name + " " + Territory.Name);
+    }
+
+    public override void Startup()
+    {
+    }
+}
