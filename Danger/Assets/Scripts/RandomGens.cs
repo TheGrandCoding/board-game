1af568d637574de269ded0a7b984e321a6523b58
CheAle14 <thedark1513@gmail.com>
2019-01-09 19:12:51 +0000

    Add functions for various dices
    Re #10

--- Danger/Assets/Scripts/RandomGens.cs
+++ Danger/Assets/Scripts/RandomGens.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace RandomGens
 {
@@ -19,12 +20,57 @@ namespace RandomGens
         {
             return random.Next(max);
         }
+        public static T Choose<T>(params T[] paramItems)
+        {
+            List<T> items = new List<T>();
+            foreach(var item in paramItems)
+            {
+                items.Add(item);
+            }
+            int index = Next(0, items.Count - 1);
+            return items[index];
+        }
+        public static T Choose<T>(IEnumerable<T> items)
+        {
+            int index = Next(0, items.Count() - 1);
+            return items.ElementAt(index);
+        }
     }
 
+    /// <summary>
+    /// Class that holds numerous dice
+    /// </summary>
     public static class Dice {
-        public static int Roll()
+        /// <summary>
+        /// Rolls a dice with n number of sides
+        /// </summary>
+        /// <param name="n">Number of sides of the dice</param>
+        /// <returns>Random number between 1 and n (excl)</returns>
+        public static int RollDiceOfSides(int n)
         {
-            return RndHelp.Next(1, 7);
+            return RndHelp.Next(1, n+1);
+        }
+        /// <summary>
+        /// Rolls a standard six sided dice
+        /// </summary>
+        public static int RollSixSided()
+        {
+            return RollDiceOfSides(6);
+        }
+        /// <summary>
+        /// Rolls a standard eight sided dice (1 to 8 incl)
+        /// </summary>
+        public static int RollEightSided()
+        {
+            return RollDiceOfSides(8);
+        }
+        /// <summary>
+        /// Rolls a six sided dice that is weighted/unfair
+        /// </summary>
+        /// <returns></returns>
+        public static int RollWeightedFourSidedDice()
+        {
+            return RndHelp.Choose(1, 2, 3, 3, 4, 4);
         }
     }
 
@@ -32,7 +78,7 @@ namespace RandomGens
     {
         public static ArmyType ThrowDice()
         {
-            var dice = Dice.Roll();
+            var dice = Dice.RollSixSided();
             switch (dice)
             {
                 case 1:
