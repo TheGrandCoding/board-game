746259d709873f9f19600ab4e753a25a3e127784
BOOseungkwan <liodjo@gmail.com>
2019-01-16 17:54:53 +0000

    start screen
    cant switch scenes though oops

---
+++ Danger/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public int scene; //manually input which scene to go to
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
